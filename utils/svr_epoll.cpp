88418cc2bc4c7f8f234caf934f6b25901ca3af3e
Joe <solkins@163.com>
2016-04-03 03:09:55 +0000

    create epoll server

---
+++ utils/svr_epoll.cpp
@@ -0,0 +1,6 @@
+#include "svr_epoll.h"
+
+svr_epoll::svr_epoll()
+{
+
+}
---
+++ utils/svr_epoll.h
@@ -0,0 +1,11 @@
+#ifndef SVR_EPOLL_H
+#define SVR_EPOLL_H
+
+
+class svr_epoll
+{
+public:
+    svr_epoll();
+};
+
+#endif // SVR_EPOLL_H
